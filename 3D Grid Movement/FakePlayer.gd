4834741baa00a20a6a0aee7149e6c846342fe19e
Dragon20C <dragon212c@gmail.com>
2021-03-10 17:04:09 +0000

    Alot better 3D Grid Based movement

--- 3D Grid Movement/FakePlayer.gd
+++ 3D Grid Movement/FakePlayer.gd
@@ -1,14 +1,43 @@
 extends KinematicBody
 
+var speed = 5
+var current_dir = Vector3()
+var next_pos = Vector3()
+var destination = Vector3()
+var up = Vector3.ZERO
+var down = Vector3.BACK
+var left = Vector3.LEFT
+var right = Vector3.RIGHT
+var can_move = false
 
+func _ready():
+	current_dir = up
+	next_pos = Vector3.FORWARD
+	destination = translation
 
 func _physics_process(delta):
+	move(delta)
 	
-	if Input.is_action_just_pressed("arrow_W"):
-		translation.z = translation.z + 1
-	elif Input.is_action_just_pressed("arrow_A"):
-		translation.x = translation.x + 1
-	elif Input.is_action_just_pressed("arrow_S"):
-		translation.z = translation.z - 1
-	elif Input.is_action_just_pressed("arrow_D"):
-		translation.x = translation.x - 1
+func move(time):
+	translation = translation.move_toward(destination,speed * time)
+	
+	if Input.is_action_just_pressed("up"):
+		next_pos = Vector3.FORWARD
+		current_dir = up
+		can_move = true
+	if Input.is_action_just_pressed("down"):
+		next_pos = Vector3.BACK
+		current_dir = down
+		can_move = true
+	if Input.is_action_just_pressed("left"):
+		next_pos = Vector3.LEFT
+		current_dir = left
+		can_move = true
+	if Input.is_action_just_pressed("right"):
+		next_pos = Vector3.RIGHT
+		current_dir = right
+		can_move = true
+	if translation.distance_to(destination) <= 0.0000:
+		if can_move:
+			destination = translation + next_pos
+			can_move = false
