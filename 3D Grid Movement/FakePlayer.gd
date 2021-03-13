59dc621da4b533bb2ee1f109c84f8fc2698c8e4e
Dragon20C <dragon212c@gmail.com>
2021-03-13 13:13:28 +0000

    Added a poor mans collision method
    For what ever reason the collision can fail if holding down a button press but tapping works fine

--- 3D Grid Movement/FakePlayer.gd
+++ 3D Grid Movement/FakePlayer.gd
@@ -6,37 +6,45 @@ var current_dir = Vector3()
 var next_pos = Vector3()
 var destination = Vector3()
 var can_move = false
+onready var collision_node = get_node("/root/World")
 onready var button_duration = $Timer
 
 func _ready():
-	current_dir = 0
+	current_dir = "up"
 	next_pos = Vector3.FORWARD
 	destination = translation
 
 func _physics_process(delta):
+	var position = Vector3()
+	position.x = int(translation.x)
+	position.z = int(translation.z)
 	translation = translation.move_toward(destination,speed * delta)
 	$Mesh.rotation.y = lerp_angle($Mesh.rotation.y, atan2(-next_pos.x, -next_pos.z), delta * angle_speed)
-	
-	if Input.is_action_pressed("up") and button_duration.is_stopped():
-		next_pos = Vector3.FORWARD
-		current_dir = "up"
-		can_move = true
-		button_duration.start()
-	if Input.is_action_pressed("down") and button_duration.is_stopped():
-		next_pos = Vector3.BACK
-		current_dir = "down"
-		can_move = true
-		button_duration.start()
-	if Input.is_action_pressed("left") and button_duration.is_stopped():
-		next_pos = Vector3.LEFT
-		current_dir = "left"
-		can_move = true
-		button_duration.start()
-	if Input.is_action_pressed("right") and button_duration.is_stopped():
-		next_pos = Vector3.RIGHT
-		current_dir = "right"
-		can_move = true
-		button_duration.start()
+	print(position)
+	if !position + Vector3.FORWARD in collision_node.collision:
+		if Input.is_action_pressed("up") and button_duration.is_stopped():
+			next_pos = Vector3.FORWARD
+			current_dir = "up"
+			can_move = true
+			button_duration.start()
+	if !position + Vector3.BACK in collision_node.collision:
+		if Input.is_action_pressed("down") and button_duration.is_stopped():
+			next_pos = Vector3.BACK
+			current_dir = "down"
+			can_move = true
+			button_duration.start()
+	if !position + Vector3.LEFT in collision_node.collision:
+		if Input.is_action_pressed("left") and button_duration.is_stopped():
+			next_pos = Vector3.LEFT
+			current_dir = "left"
+			can_move = true
+			button_duration.start()
+	if !position + Vector3.RIGHT in collision_node.collision:
+		if Input.is_action_pressed("right") and button_duration.is_stopped():
+			next_pos = Vector3.RIGHT
+			current_dir = "right"
+			can_move = true
+			button_duration.start()
 		
 	if translation.distance_to(destination) <= 0.0000 and can_move:
 		destination = translation + (next_pos)
--- 3D Grid Movement/World.gd
+++ 3D Grid Movement/World.gd
@@ -4,17 +4,30 @@ var grid_size = 30
 var offset = 2
 onready var tile = preload("res://Ground.tscn")
 onready var player = preload("res://FakePlayer.tscn")
-
+onready var object = preload("res://Object.tscn")
+var collision = []
 func _ready():
+	randomize()
 	generate_grid()
+	generate_objects()
 	generate_player()
-	
+	#print(collision)
 func generate_grid():
 	for x in range(grid_size):
 		for y in range(grid_size):
 			var instance = tile.instance()
 			add_child(instance)
 			instance.translation = Vector3(x,0,y) - Vector3(grid_size/2,0,grid_size/2)
+
+func generate_objects():
+	for x in range(grid_size):
+		for y in range(grid_size):
+			var RNG = randi() % 100 - 1 
+			if RNG < 5:
+				var instance = object.instance()
+				add_child(instance)
+				instance.translation = Vector3(x,0,y) - Vector3(grid_size/2,0,grid_size/2)
+				collision.append(instance.translation)
 
 func generate_player():
 	var instance = player.instance()
