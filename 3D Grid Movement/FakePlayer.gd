c39d3c6a7390e983a753788dc6551dec3a4d87e3
Dragon20C <dragon212c@gmail.com>
2021-03-13 19:04:52 +0000

    Not glitching any more?!?!
    Not sure why but it no longer glitches out might be a timer/physics bug or issue

--- 3D Grid Movement/FakePlayer.gd
+++ 3D Grid Movement/FakePlayer.gd
@@ -15,31 +15,28 @@ func _ready():
 	destination = translation
 
 func _physics_process(delta):
-	var position = Vector3()
-	position.x = int(translation.x)
-	position.z = int(translation.z)
 	translation = translation.move_toward(destination,speed * delta)
 	$Mesh.rotation.y = lerp_angle($Mesh.rotation.y, atan2(-next_pos.x, -next_pos.z), delta * angle_speed)
-	print(position)
-	if !position + Vector3.FORWARD in collision_node.collision:
+	
+	if !translation + Vector3.FORWARD in collision_node.collision:
 		if Input.is_action_pressed("up") and button_duration.is_stopped():
 			next_pos = Vector3.FORWARD
 			current_dir = "up"
 			can_move = true
 			button_duration.start()
-	if !position + Vector3.BACK in collision_node.collision:
+	if !translation + Vector3.BACK in collision_node.collision:
 		if Input.is_action_pressed("down") and button_duration.is_stopped():
 			next_pos = Vector3.BACK
 			current_dir = "down"
 			can_move = true
 			button_duration.start()
-	if !position + Vector3.LEFT in collision_node.collision:
+	if !translation + Vector3.LEFT in collision_node.collision:
 		if Input.is_action_pressed("left") and button_duration.is_stopped():
 			next_pos = Vector3.LEFT
 			current_dir = "left"
 			can_move = true
 			button_duration.start()
-	if !position + Vector3.RIGHT in collision_node.collision:
+	if !translation + Vector3.RIGHT in collision_node.collision:
 		if Input.is_action_pressed("right") and button_duration.is_stopped():
 			next_pos = Vector3.RIGHT
 			current_dir = "right"
@@ -47,6 +44,7 @@ func _physics_process(delta):
 			button_duration.start()
 		
 	if translation.distance_to(destination) <= 0.0000 and can_move:
+		destination = translation + (next_pos)
 		destination = translation + (next_pos)
 		can_move = false
 
